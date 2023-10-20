8d95c341745ec8e465f76c7a5ddcbddc942a8964
sojstyles <olasoji@olasojiopeyemi.com>
2023-10-20 07:16:28 -0400

    Configure remote state

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+
+  backend "s3" {
+    bucket         = "mydevopsbackup"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-remote-state"
+  }
 }
 
 # Configure the AWS Provider
