d999d25e8b42214270192a1cfa5d267e5ba5d1fc
sojstyles <olasoji@olasojiopeyemi.com>
2023-10-22 08:40:18 -0400

    Add level2

--- level2/provider.tf
+++ level2/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" {
     bucket         = "mydevopsbackup"
-    key            = "terraform.tfstate"
+    key            = "level2.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-remote-state"
   }
--- level2/variables.tf
+++ level2/variables.tf
@@ -6,11 +6,3 @@ variable "env_code" {
 variable "vpc_cidr" {
 
 }
-
-variable "private_cidr" {
-
-}
-
-variable "public_cidr" {
-
-}
